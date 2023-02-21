8924a0ad0372f210020177c7e9450502972f853b
CitrixQian <sijieqian@foxmail.com>
2023-02-21 23:52:42 +0800

    init

--- src/main/java/com/github/greengerong/juice/V4/ConcertExampleV2.java
+++ src/main/java/com/github/greengerong/juice/V4/ConcertExampleV2.java
@@ -4,9 +4,9 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class ConcertExampleV2 {
-  public static void main(String[] args) {
-    Injector i = Guice.createInjector(new ConcertModuleV2());
-    ConcertHallV2 hall = i.getInstance(ConcertHallV2.class);
-    System.out.println(hall);
-  }
+    public static void main(String[] args) {
+        Injector i = Guice.createInjector(new ConcertModuleV2());
+        ConcertHallV2 hall = i.getInstance(ConcertHallV2.class);
+        System.out.println(hall);
+    }
 }
--- src/main/java/com/github/greengerong/juice/recipes/Main.java
+++ src/main/java/com/github/greengerong/juice/recipes/Main.java
@@ -5,18 +5,15 @@ import com.google.inject.*;
 import java.util.List;
 
 public class Main {
-  public static void main(String[] args) {
-    Injector i =
-        Guice.createInjector(
-            new AbstractModule() {
-              protected void configure() {
-                bind(new TypeLiteral<List<String>>() {})
-                    .toProvider(new FortuneListProvider())
-                    .in(Singleton.class);
+    public static void main(String[] args) {
+        Injector i = Guice.createInjector(new AbstractModule() {
+            protected void configure() {
+                bind(new TypeLiteral<List<String>>() {
+                }).toProvider(new FortuneListProvider()).in(Singleton.class);
                 bind(FortuneService.class).to(FortuneServiceImpl.class);
-              }
-            });
-    FortuneService fortuneService = i.getInstance(FortuneService.class);
-    System.out.println(fortuneService.randomFortune());
-  }
+            }
+        });
+        FortuneService fortuneService = i.getInstance(FortuneService.class);
+        System.out.println(fortuneService.randomFortune());
+    }
 }
